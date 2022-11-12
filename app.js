028640e8cd89a8b20045cd82861003de427744f1
Tauheed_Siddiqui <tauheedsiddiqui8760@gmail.com>
2022-11-12 20:30:56 +0530

    Forgot Password Complete

--- app.js
+++ app.js
@@ -6,16 +6,20 @@ const dotenv=require('dotenv');
 
 const bodyParser = require("body-parser");
 
+
+// Backend Routes
 const sequelize = require("./util/database");
 const userRoutes=require('./routes/user')
 const expenseRoutes=require('./routes/expense')
 const purchaseRoutes = require('./routes/purchase')
-
+const resetPasswordRoutes = require('./routes/resetpassword');
 const app = express();
 
+// Models
 const User=require('./models/user');
 const Expense=require('./models/expenseAdd');
 const Order=require('./models/orders');
+const Forgotpassword = require('./models/forgotpassword');
 
 
 app.use(cors());
@@ -24,9 +28,12 @@ dotenv.config();
 // app.use(bodyParser.urlencoded({extended:false}));//thsi is for handling forms 
 app.use(bodyParser.json());  //this is for handling jsons
 
+// Frontend routes
 app.use(userRoutes);
 app.use(expenseRoutes);
-app.use('/purchase', purchaseRoutes)
+app.use('/purchase', purchaseRoutes);
+app.use('/password', resetPasswordRoutes);
+
 
 
 //Associations
@@ -35,6 +42,9 @@ Expense.belongsTo(User);
 
 User.hasMany(Order);
 Order.belongsTo(User);
+
+User.hasMany(Forgotpassword);
+Forgotpassword.belongsTo(User);
 
 sequelize
   // .sync({ force: true })
--- views/forgot.js
+++ views/forgot.js
@@ -1,3 +1,5 @@
+// const sgMail = require('@sendgrid/mail')
+
 function forgotPassword(e) {
   e.preventDefault();
   console.log(e.target.name);
@@ -8,7 +10,7 @@ function forgotPassword(e) {
   };
   console.log(userDetails);
   axios
-    .post("http://localhost:3000/forgotpassword", userDetails)
+    .post("http://localhost:3000/password/forgotpassword", userDetails)
     .then((response) => {
       if (response.status === 202) {
         document.body.innerHTML +=
@@ -21,3 +23,20 @@ function forgotPassword(e) {
       document.body.innerHTML += `<div style="color:red;">${err} <div>`;
     });
 }
+
+// sgMail.setApiKey(process.env.SENDGRID_API_KEY)
+// const msg = {
+//   to: 'tauheedsiddiqui8760@gmail.com', // Change to your recipient
+//   from: 'mohammadtauheedbly@gmail.com', // Change to your verified sender
+//   subject: 'Sending with SendGrid is Fun',
+//   text: 'and easy to do anywhere, even with Node.js',
+//   html: '<strong>and easy to do anywhere, even with Node.js</strong>',
+// }
+// sgMail
+//   .send(msg)
+//   .then(() => {
+//     console.log('Email sent')
+//   })
+//   .catch((error) => {
+//     console.error(error)
+//   })
