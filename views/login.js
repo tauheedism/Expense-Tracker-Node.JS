679983a01004ca380bd29edf69aa618342452b56
Tauheed_Siddiqui <tauheedsiddiqui8760@gmail.com>
2022-11-12 13:31:55 +0530

    Forgot Password frontend Request only

--- views/login.js
+++ views/login.js
@@ -1,21 +1,26 @@
-function login(e){
-    e.preventDefault();
-    // console.log(e.target.name);
-    const loginDetails={
-        email:e.target.email.value,
-        password:e.target.password.value,
-    }
-    console.log(loginDetails);
-    axios.post('http://localhost:3000/login',loginDetails)
-    .then((response)=>{
-        alert(response.data.message);
-        localStorage.setItem('token',response.data.token)
-        window.location="expense.html"
+function login(e) {
+  e.preventDefault();
+  // console.log(e.target.name);
+  const loginDetails = {
+    email: e.target.email.value,
+    password: e.target.password.value,
+  };
+  console.log(loginDetails);
+  axios
+    .post("http://localhost:3000/login", loginDetails)
+    .then((response) => {
+      alert(response.data.message);
+      localStorage.setItem("token", response.data.token);
+      window.location = "expense.html";
     })
-    .catch(err=>{
-        console.log(JSON.stringify(err))
-        document.body.innerHTML+=`<div style="color:red";>${err.message} </div>`
-    })
-    e.target.email.value='';
-    e.target.password.value='';
+    .catch((err) => {
+      console.log(JSON.stringify(err));
+      document.body.innerHTML += `<div style="color:red";>${err.message} </div>`;
+    });
+  e.target.email.value = "";
+  e.target.password.value = "";
+}
+
+function forgotPassword() {
+  window.location = "forgot.html";
 }
